942a409e0c0b2de0fe8118ef1d22916c68aacdcb
데이터베이스 <127382035+crystal6520@users.noreply.github.com>
2023-04-04 11:08:36 +0900

    Add files via upload

---
+++ Chapter08/src/sec02/ex02/MemberServlet.java
@@ -0,0 +1,47 @@
+package sec02.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/Member8")
+public class MemberServlet extends HttpServlet {
+
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doHandle(request, response);
+   }
+   @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doHandle(request, response);
+   }
+   
+   protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      
+      request.setCharacterEncoding("utf-8");
+      response.setContentType("text/html;charset=utf-8");
+      PrintWriter out = response.getWriter();
+      MemberDAO dao = new MemberDAO();
+      
+      //추가
+      List memberList = dao.listMembers();
+      
+      request.setAttribute("memberList", memberList);
+      RequestDispatcher dispatch = request.getRequestDispatcher("viewMembers");
+      dispatch.forward(request, response);
+   }
+}
